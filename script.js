3aef373366237eb2bcc2fa08b35a33461a89b467
Nobreganj <57805973+Nobreganj@users.noreply.github.com>
2021-07-26 12:14:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,14 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "Jul 26, 2021 12:00:00",
-      countDown = new Date(birthday).getTime(),
+  let birthday = "2021-07-26T12:00:00.000Z";
+  birthdayUtc = Date.parse(birthday);
+  nowUtc = Date.parse(new Date().toISOString());
+      //countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
-        let now = new Date().getTime(),
-            distance = countDown - now;
-
+        //let now = new Date().getTime(),
+            distance = birthdayUtc - nowUtc;
         document.getElementById("days").innerText = Math.floor(distance / (day)),
           document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
           document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
